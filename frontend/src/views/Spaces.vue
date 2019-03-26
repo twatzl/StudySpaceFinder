22106e6aec746c503746fd198353181861189a27
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2019-03-26 14:41:20 +0200

    add axios and json-server

--- frontend/src/views/Spaces.vue
+++ frontend/src/views/Spaces.vue
@@ -18,6 +18,14 @@
 import { Component, Vue } from "vue-property-decorator";
 import StudySpace from "@/components/StudySpace.vue"; // @ is an alias to /src
 import Toolbar from "@/components/Toolbar.vue";
+import axios from 'axios';
+
+axios
+  .get('http://localhost:3001/spaces')
+  .then(response => {
+    const spaces = response.data
+    console.log(spaces)
+  })
 
 @Component({
   components: {
