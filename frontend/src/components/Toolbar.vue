c1dd12624977a04da7312adf26474ad8e8f3baa9
Tobias Watzl <twatzl@gmx.at>
2019-04-04 14:17:29 +0200

    First version of the search

--- frontend/src/components/Toolbar.vue
+++ frontend/src/components/Toolbar.vue
@@ -1,23 +1,51 @@
 <template>
-  <div>
-    <md-toolbar class="md-primary">
-      <h3 class="md-title" style="flex: 1">Study spaces</h3>
-      <md-button class="md-icon-button">
-        <md-icon>more_vert</md-icon>
-      </md-button>
-    </md-toolbar>
-  </div>
+    <div>
+        <md-toolbar class="md-primary">
+            <h3 class="md-title" style="flex: 1">Study spaces</h3>
+
+            <md-autocomplete
+                    class="search"
+                    v-model="searchStringInternal"
+                    :md-options="searchAutocompleteOptions"
+                    :md-open-on-focus="false"
+                    v-on:md-changed="searchStringChanged()"
+                    md-layout="box">
+                <label>Search...</label>
+            </md-autocomplete>
+
+
+            <md-button class="md-icon-button">
+                <md-icon>more_vert</md-icon>
+            </md-button>
+        </md-toolbar>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-  .md-toolbar + .md-toolbar {
-    margin: 16px 0;
-  }
+    .md-toolbar + .md-toolbar {
+        margin: 16px 0;
+    }
+    .search {
+        max-width: 500px;
+    }
 </style>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
 
-@Component
-export default class StudySpace extends Vue {}
+    @Component
+    export default class StudySpace extends Vue {
+        private searchStringInternal: any = null;
+
+        @Prop({default: []})
+        private searchAutocompleteOptions!: string[];
+
+        @Prop({default: []})
+        private availableTags!: string[];
+
+        private searchStringChanged() {
+            const tb = this;
+            this.$emit('search-changed', tb.searchStringInternal);
+        }
+    }
 </script>
--- frontend/src/views/Spaces.vue
+++ frontend/src/views/Spaces.vue
@@ -1,67 +1,138 @@
 <template>
-  <div id="spaces">
-    <Toolbar/>
-    <div class="md-layout md-alignment-center">
-      <StudySpace 
-        v-for="item in spaces" 
-        v-bind:key="item.id"
-        v-bind:space="item"
-      ></StudySpace>
+    <div id="spaces">
+        <Toolbar v-bind:searchAutocompleteOptions="autocompleteOptions" v-on:search-changed="onSearchChanged"/>
+        <div class="md-layout md-alignment-center">
+            <div v-for="item in filteredSpaces">
+                <StudySpace
+                        v-bind:key="item.id"
+                        v-bind:space="item"
+                ></StudySpace>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style lang="scss" scoped>
-.md-alignment-center {
-  align-items: flex-start;
-}
+    .md-alignment-center {
+        align-items: flex-start;
+    }
 </style>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import StudySpace from '@/components/StudySpace.vue'; // @ is an alias to /src
-import Toolbar from '@/components/Toolbar.vue';
-import axios from 'axios';
+    import {Component, Vue} from 'vue-property-decorator';
+    import StudySpace from '@/components/StudySpace.vue'; // @ is an alias to /src
+    import Toolbar from '@/components/Toolbar.vue';
+    import axios from 'axios';
 
-interface Space {
-  id: number;
-  name: string;
-  building: string;
-  open: string;
-  people: string;
-  reservable: boolean;
-  photo?: string;
-  floor?: string;
-  info?: string;
-  tags?: string[];
-}
+    interface Space {
+        id: number;
+        name: string;
+        building: string;
+        open: string;
+        people: string;
+        reservable: boolean;
+        photo?: string;
+        floor?: string;
+        info?: string;
+        tags?: string[];
+    }
 
-axios
-  .get('http://localhost:3001/spaces')
-  .then((response) => {
-    const spaces = response.data;
-    // console.log(spaces)
-  });
-
-@Component({
-  components: {
-    StudySpace, Toolbar,
-  },
-})
-
-export default class Spaces extends Vue {
-  private spaces: Space[] = [];
-  private mounted() {
     axios
-      .get('http://localhost:3001/spaces')
-      .then((response) => {
-        response.data.forEach((element) => {
-          const space = element as Space;
-          this.spaces.push(space);
+        .get('http://localhost:3001/spaces')
+        .then((response) => {
+            const spaces = response.data;
+            // console.log(spaces)
         });
-      });
-    // console.log(this.spaces)
-  }
-}
 
+    @Component({
+        components: {
+            StudySpace, Toolbar,
+        },
+    })
+
+    export default class Spaces extends Vue {
+
+        private static isNullOrUndefined(obj) {
+            return obj == null;
+        }
+
+        private searchString: string = '';
+        private spaces: Space[] = [];
+        private filteredSpaces: Space[] = [];
+        private autocompleteOptions: string[] = [];
+        private availableTags: string[] = [];
+        private selectedTags: string[] = [];
+
+        private mounted() {
+            this.availableTags = [];
+            axios
+                .get('http://localhost:3001/spaces')
+                .then((response) => {
+                    response.data.forEach((element) => {
+                        const space = element as Space;
+                        this.spaces.push(space);
+                        this.addSpaceToSearchIndex(space);
+                    });
+
+                    this.updateFilteredSpaces();
+                });
+            // console.log(this.spaces)
+        }
+
+        private addSpaceToSearchIndex(space: Space) {
+            this.autocompleteOptions.push(space.name + ', ' + space.building + ', ' + space.floor);
+            if (!Spaces.isNullOrUndefined(space.tags)) {
+                const tags = space.tags as string[];
+                this.autocompleteOptions = this.autocompleteOptions.concat(tags);
+                this.availableTags = this.availableTags.concat(tags);
+            }
+        }
+
+        private updateFilteredSpaces() {
+            const fSpaces: Space[] = [];
+            for (const sName in this.spaces) {
+                const s = this.spaces[sName];
+                if (this.spaceMatchesSearch(s)) {
+                    fSpaces.push(s);
+                }
+            }
+            this.filteredSpaces = fSpaces;
+        }
+
+        private onSearchChanged(searchString: string) {
+            this.searchString = searchString;
+            this.updateFilteredSpaces();
+        }
+
+        private spaceMatchesSearch(space: Space) {
+            // if (space == null) return true; // debug
+            /*
+            if (this.selectedTags.length > 0) {
+                for (let sTIdx in this.selectedTags) {
+                    let selectedTag = this.selectedTags[sTIdx];
+                    let found = false;
+                    for (let tIdx in space.tags) {
+                        let tag = space.tags[tIdx];
+                        if (tag == selectedTag) {
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    // do not display a study space if any of the search tags does not apply
+                    if (!found) return false;
+                }
+            }
+            */
+
+            if (this.searchString != null && this.searchString.length > 0) {
+                const lowerSearch = this.searchString.toLowerCase();
+
+                return space.name != null && space.name.toLowerCase().includes(lowerSearch) ||
+                    space.building != null && space.building.toLowerCase().includes(lowerSearch) ||
+                    space.floor != null && space.floor.toLowerCase().includes(lowerSearch);
+            }
+            return true;
+        }
+    }
 </script>
