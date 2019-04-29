0bfd34f172808e4cf194b8df6b310afff21d5841
Mari Trtnik <maripartan@gmail.com>
2019-04-29 18:06:20 +0300

    crude version of multiselect filtering

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
+import Multiselect from'vue-multiselect';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -8,7 +9,7 @@ import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
 Vue.use(VueMaterial);
-
+Vue.component('multiselect', Multiselect)
 Vue.config.productionTip = false;
 
 new Vue({
